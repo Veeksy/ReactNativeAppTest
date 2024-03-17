cdcc5fbd8c429017a3d29ec83a44552c110906fa
VeekSY <63778404+Xamnack@users.noreply.github.com>
2024-03-17 23:00:04 +0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>лерусег я тебя люблю</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
